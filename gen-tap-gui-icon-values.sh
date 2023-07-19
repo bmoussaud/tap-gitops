daf55100391bb9a59a98c26aa20dbd3c8e7ddefb
Benoit Moussaud <benoit@moussaud.org>
2023-07-19 16:15:20 +0200

    ytt template to generate gui

--- gen-tap-gui-icon-values.sh
+++ gen-tap-gui-icon-values.sh
@@ -1,18 +1,7 @@
-
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
-set -o xtrace
+#set -o xtrace
+TAP_ICON_BASE64=$(base64 -i clusters/${CLUSTER_NAME}/cluster-config/tap-logo.png) ytt -f templates/tap-gui-icon-values.yaml --data-values-file clusters/${CLUSTER_NAME}/cluster-config/values/tap-install-values.yaml --data-values-env TAP > clusters/${CLUSTER_NAME}/cluster-config/values/tap-gui-icon-values.yaml
 
-ICON_BASE64=$(base64 -i clusters/${CLUSTER_NAME}/cluster-config/tap-logo.png)
 
-cat > clusters/${CLUSTER_NAME}/cluster-config/values/tap-gui-icon-values.yaml << EOF
-tap_install:
-  values:
-    tap_gui:
-      app_config:        
-        customize:
-          custom_logo: "${ICON_BASE64}"
-          custom_name: "Micropets Tanzu Application Platform"
-          
-EOF
 
