a94eb48907e313f38625908dcd67010ab34ea324
Benoit Moussaud <benoit@moussaud.org>
2023-09-27 11:15:28 +0200

    upgrade tap sh

---
+++ upgrade-tap.sh
@@ -0,0 +1,5 @@
+VERSION=$1
+./copy_tap_package.sh akseutap6registry ${VERSION}
+make gen-install-values gen-tap-gui-icon-values  TAP_VERSION=${VERSION}
+git commit -am "upgrade TAP ${VERSION}"
+git push
