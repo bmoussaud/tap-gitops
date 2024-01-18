452d414c25ffa46cd7396c1650278f2b107c545c
Benoit Moussaud <benoit@moussaud.org>
2024-01-18 10:08:18 +0100

    encrypt

--- encrypt_sops.sh
+++ encrypt_sops.sh
@@ -1,9 +1,10 @@
-//set -x
+#set -x
 echo "SOPS Encrypt $1"
 export SOPS_AGE_KEY_FILE=~/dotconfig/tapkey.txt
 export SOPS_AGE_KEY=$(cat ${SOPS_AGE_KEY_FILE})
 export SOPS_AGE_RECIPIENTS=$(cat ${SOPS_AGE_KEY_FILE} | grep "# public key: " | sed 's/# public key: //')
-echo "$1\n" >>.gitignore
+echo "$1" >>.gitignore
+echo " " >>.gitignore
 cat .gitignore | sort -u >.gitignore.temp
 mv .gitignore.temp .gitignore
 sops --encrypt $1 >$1.sops.yaml
