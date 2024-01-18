d5ec268c207de9cd032a767950f7d4b71b2f85d9
Benoit Moussaud <benoit@moussaud.org>
2024-01-18 09:57:35 +0100

    encrypt azure-secret-sp-sensitive

--- encrypt_sops.sh
+++ encrypt_sops.sh
@@ -1,8 +1,9 @@
+//set -x
 echo "SOPS Encrypt $1"
 export SOPS_AGE_KEY_FILE=~/dotconfig/tapkey.txt
 export SOPS_AGE_KEY=$(cat ${SOPS_AGE_KEY_FILE})
-export SOPS_AGE_RECIPIENTS=`cat ${SOPS_AGE_KEY_FILE} | grep "# public key: " | sed 's/# public key: //'`
-echo "$1\n" >> .gitignore
-cat .gitignore | sort -u > .gitignore.temp
+export SOPS_AGE_RECIPIENTS=$(cat ${SOPS_AGE_KEY_FILE} | grep "# public key: " | sed 's/# public key: //')
+echo "$1\n" >>.gitignore
+cat .gitignore | sort -u >.gitignore.temp
 mv .gitignore.temp .gitignore
-sops --encrypt $1 > $1.sops.yaml
+sops --encrypt $1 >$1.sops.yaml
